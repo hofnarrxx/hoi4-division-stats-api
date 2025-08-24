5c58a4e2ba58375ad2a4358a4dc203896bb31664
hofnarrxx <maksymparasiun@gmail.com>
2025-08-24 11:55:02 +0200

    game data for 1.16.9

---
+++ src/main/java/com/hofnarrxx/hoi4_unit_stats_api/Hoi4UnitStatsApiApplication.java
@@ -0,0 +1,13 @@
+package com.hofnarrxx.hoi4_unit_stats_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hoi4UnitStatsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hoi4UnitStatsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hofnarrxx/hoi4_unit_stats_api/Hoi4UnitStatsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hofnarrxx.hoi4_unit_stats_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hoi4UnitStatsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
